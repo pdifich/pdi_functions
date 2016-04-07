c97bc6e7d5d7fad3034a532c610e6af807510632
aldo_bedrij <wbedrij@gmail.com>
2016-04-07 10:53:40 -0300

    script para compilar contra opencv

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+	echo "Usage $0 file.cpp" >&2
+	echo "Construye el ejecutable file.bin" >&2
+	exit 1
+fi
+
+file=$1
+g++ "${file}" -W{all,extra,pedantic} -I./pdi_functions -c -ggdb -std=c++11 -o "${file%.cpp}.o"
+g++ "${file%.cpp}.o" $(pkg-config --libs opencv) -o "${file%.cpp}.bin"
